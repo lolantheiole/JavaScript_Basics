b7d360e53ee9f94a215ba268729c79a1e0e5e9a5
lolantheiole <damacm1111@DAs-MacBook-Air-8.local>
2023-11-06 11:56:23 +0200

    modified the arrays file and did functions

--- arrays.js
+++ arrays.js
@@ -5,12 +5,13 @@ let courses = ["React", "Angular 2", "Javascript", "html"]; //pre-defined array
 
 //Declaring variable
 let i;
-let space = " ";
 let len = courses.length;
+let space = " ";
+
 
 //pushing an item to the array
 courses.push("c++");
-console.log("List of items:" + " " + courses);
+//console.log("List of items:" + " " + courses);
 
 
 //initializing and declaring arrays
@@ -21,11 +22,13 @@ learners[2] = 3;
 learners[3] = 4;
 
 //normally it shouldn print an OUT OF BOUND error
-console.log("Number of Leaners:" + learners)
+//console.log("Number of Leaners:" + learners)
 
 //printing using a for loop 
 for (i = 0; i < len; i++) {
-    space += courses[i + '']
- }
- console.log(courses);
- console.log(space);
+
+    //space += courses[i]+ " ";
+    space = space + courses[i] + " ";
+}
+console.log(courses);
+console.log(space);
---
+++ functions.js
@@ -0,0 +1,7 @@
+let courses = ["Java", "Ruby", "Vue"]
+
+courses.forEach(func)
+
+function func(course) {
+    console.log(course)
+}
